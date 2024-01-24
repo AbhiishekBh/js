e396da277facd07f0c25662b3c8313b0c182cd8c
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-01-24 17:46:22 +0000

    operations

--- Basics/03_conversionoperation.js
+++ Basics/03_conversionoperation.js
@@ -3,7 +3,7 @@ let score1 = null;
 // let score3 = "22abc";
  // let score4 = undefined; // or (let score;)
 
-console.log(typeof score1);
+// console.log(typeof score1);
 /* console.log(typeof SCORE);
 console.log(typeof score2);
 console.log(typeof score3);
@@ -16,8 +16,8 @@ let myValueInNumber4 = Number(score4);*/
 
  /* note whenever we do conversion in datatypes always the datatype start with the capital letter ex..Number,String etc....  */
 
-console.log(typeof myValueInNumber1);
-console.log(myValueInNumber1); //for null it will return 0
+// console.log(typeof myValueInNumber1);
+// console.log(myValueInNumber1); //for null it will return 0
 
 /* console.log(typeof myValueInNumber2);
 console.log(myValueInNumber2); //it will return 22
@@ -31,3 +31,34 @@ console.log(myValueInNumber4); //NaN
 
 /* there are many little bugs in javascript thats wy people are using typescript understand the concept 
 */
+
+
+
+// ******************** OPERATIONS************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// ** power operation , % gives remainder
+
+//console.log(+true); // will give 1
+// console.log(true+); // throw an error (f*ck around and find out)...
+//console.log(+""); // will return 0 it is like (+false) because we have alredy seen that value of "" => false previosly....
+
+// just for knowledge purpose
+
+let gameCounter = 100;
+gameCounter++; // prefix ++ and postfix ++ differnce in pre value increase before usage and in post value increase after usage.....
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// https://tc39.es/ecma262/
+// https://tc39.es/ecma262/#sec-type-conversion
+
+
+
+
+
+
+
