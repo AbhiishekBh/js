8b133218d304ba665afb760779ac1c968670d8cb
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-01-24 16:12:41 +0000

    conversion and operation

---
+++ Basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+let score1 = null;
+// let score2 = "22";
+// let score3 = "22abc";
+ // let score4 = undefined; // or (let score;)
+
+console.log(typeof score1);
+/* console.log(typeof SCORE);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4); */
+
+let myValueInNumber1  = Number(score1);
+/*let myValueInNumber2  = Number(score2);
+let myValueInNumber3  = Number(score3); 
+let myValueInNumber4 = Number(score4);*/
+
+ /* note whenever we do conversion in datatypes always the datatype start with the capital letter ex..Number,String etc....  */
+
+console.log(typeof myValueInNumber1);
+console.log(myValueInNumber1); //for null it will return 0
+
+/* console.log(typeof myValueInNumber2);
+console.log(myValueInNumber2); //it will return 22
+
+console.log(typeof myValueInNumber3);
+console.log(myValueInNumber3); // it will not ruturn any interger bucause the input is 22abc it will return NaN(not a number)
+
+console.log(typeof myValueInNumber4);
+console.log(myValueInNumber4); //NaN 
+*/
+
+/* there are many little bugs in javascript thats wy people are using typescript understand the concept 
+*/
