b81a8caa71ad501e568d8149a55cc7a1b4e5ca89
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-01-24 11:25:54 +0000

    variables let const and var

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,1 +1,14 @@
 const accountId = "abcdef";
+let nuMber = 445;
+nuMber= 34;
+// accountId = "uuuu";
+road="new highway";
+console.log(accountId);
+console.log(nuMber);
+let gender; // its value will be undefined....
+console.table([accountId,nuMber,road,gender]);
+/* use let and const in your code and preffer not to use var because block scope and functional scope
+okkk bro */
+/* and also always use variable type like var , const ,let before declaring variable beacause in javascript  you can declare it without declaring 
+varible type and will reserve the memory of that variable butttttttt this is bad practice   */
+// in javascript you can use semicolon or not (;) it depend on you it will not generate errors.....
