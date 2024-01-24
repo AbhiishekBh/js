035a83543aad192bf2a5e9cd79b4e2fa77cf0c30
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-01-24 12:02:46 +0000

    about DataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,56 @@
+// there is lot of differnce btw earlier js and present time js. In earlier time js there is no class , module,arrow function etc...
+// but now these things are in js...
+
+
+"use strict"; // now by using that your code will be treated as newer version of js 
+//after using use strict the is no method like "no STRICt" etc your all code will be treated as the newer version of js keep that in mind
+ 
+
+//   alert("hello") 
+//while in node you can not use alert directly like you use in console
+//we are using node js not browser
+//alert use for popping the window
+
+
+/*
+
+for documentation use MDN like search alertMDN  {MDN}
+real documentation and rules is by ECMA organization  (https://tc39.es/)
+
+*/
+
+
+let age = 33; // number
+const name = "arun"; //String
+let value = false; //boolean
+let state = null; //null(empty)
+
+//number => 2 to the power 53/52
+//bigInt => for big value that can't store in number
+//String => ""
+// boolean => true/false
+//null => standLONE value also datatype
+//undefined => undefined is like value is not defined
+// symbol => unique mostly used in react or in others for definig uniqueness...
+
+console.log(typeof(name)); //String
+console.log(typeof age)  //Number
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined;
+
+
+
+// objects  **
+
+
+
+
+
+
+
+
+
+
+
+//you can use ; or notdepend on you in js this will not effect you code
